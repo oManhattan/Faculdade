c0e0363b2e90f56b6216805c520ab84697922b19
oManhattan <matheusbatuque@gmail.com>
2022-02-11 20:19:26 -0300

    .

---
+++ 3_Semestre/POO/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    
+    public void go() {
+        System.out.println("Ola Mundo");
+    }
+
+    public static void main(String[] args) {
+        Hello hi = new Hello();
+
+        hi.go();
+    }
+}
