67b346dd7184befcabee06f607d18ffe66b9d118
Manhattan <matheusbatuque@gmail.com>
2022-03-11 02:16:43 -0300

    .

--- 3_Semestre/Estrutura_Dados/Ado01/Operacoes.java
+++ 3_Semestre/Estrutura_Dados/Ado01/Operacoes.java
@@ -1,6 +1,6 @@
 public class Operacoes {
 
-    private static Livro[] lista = new Livro[5];
+    private static Livro[] lista = new Livro[1];
     private static int n = 0;
 
     /**
@@ -44,7 +44,7 @@ public class Operacoes {
         }
 
         if (indice != -1) {
-            for (int i = indice; i < n - 1 ; i++) {
+            for (int i = indice; i < n - 1; i++) {
                 lista[i] = lista[i + 1];
             }
 
@@ -52,100 +52,33 @@ public class Operacoes {
         }
     }
 
-<<<<<<< Updated upstream
     /**
-     * Organiza o array pelo id de cada livro
+     * Organiza o array por titulo
      */
-    public static void sortById() {
-        mergeSort(0, lista.length, lista);
-    }
-
-    /**
-     * Organiza o array em ordem alfabética e por autor
-     */
-    public static void sortByTitle() {
+    public static void sortByTitulo() {
         mergeString(0, lista.length, lista);
-=======
-    public static Livro[] sortById() {
-        quickSort(lista, 0, lista.length - 1);
-        return lista;
     }
 
-    // public static Livro[] sortByAutor(Livro[] l) {
-
-    // }
-
-    // public static Livro[] sortByTitulo(Livro[] l) {
-
-    // }
-
-    // public static Livro[] sortByPreco(Livro[] l) {
-
-    // }
-
-    private static void quickSort(Livro[] vetor, int inicio, int fim) {
-        if (fim < inicio) {
-            int indexPivo = dividir(vetor, inicio, fim);
-
-            quickSort(vetor, inicio, indexPivo - 1);
-            quickSort(vetor, indexPivo + 1, fim);
-        }
+    public static int searchTitle() {
+        
     }
 
-    private static int dividir(Livro[] vetor, int inicio, int fim) {
-        int pivo, pontEsq, pontDir = fim;
-        pontEsq = inicio + 1;
-        pivo = vetor[inicio].getId();
-
-        while (pontEsq <= pontDir) {
-
-            while (pontEsq <= pontDir && vetor[pontEsq].getId() <= pivo) {
-                pontEsq++;
-            }
-
-            while (pontDir >= pontEsq && vetor[pontDir].getId() > pivo) {
-                pontDir--;
-            }
-
-            if (pontEsq < pontDir) {
-                trocar(vetor, pontDir, pontEsq);
-                pontEsq++;
-                pontDir--;
-            }
-        }
-
-        trocar(vetor, inicio, pontDir);
-        return pontDir;
-    }
-
-    private static void trocar(Livro[] vetor, int i, int j) {
-        Livro temp = vetor[i];
-        vetor[i] = vetor[j];
-        vetor[j] = temp;
->>>>>>> Stashed changes
-    }
-
+    /**
+     * Adiciona mais esapaço ao array para adicionar mais Livro()
+     * @return
+     */
     private static Livro[] alocarNovoArray() {
         Livro[] novaLista = new Livro[lista.length + 4];
         System.arraycopy(lista, 0, novaLista, 0, lista.length);
         return novaLista;
     }
 
-    private static void mergeSort(int inicio, int tamanho, Livro[] vetor) {
-        if (inicio < tamanho - 1) {
-            int meio = (inicio + tamanho) / 2;
-            mergeSort(inicio, meio, vetor);
-            mergeSort(meio, tamanho, vetor);
-            intercalarString(inicio, meio, tamanho, vetor);
-        }
-    }
-
     private static void mergeString(int inicio, int tamanho, Livro[] vetor) {
         if (inicio < tamanho - 1) {
             int meio = (inicio + tamanho) / 2;
-            mergeSort(inicio, meio, vetor);
-            mergeSort(meio, tamanho, vetor);
-            intercalar(inicio, meio, tamanho, vetor);
+            mergeString(inicio, meio, vetor);
+            mergeString(meio, tamanho, vetor);
+            intercalarString(inicio, meio, tamanho, vetor);
         }
     }
 
@@ -160,48 +93,8 @@ public class Operacoes {
         j = meio;
         k = 0;
 
-        while(i < meio && j < tamanho) {
-            if(vetor[i].getTitulo().compareToIgnoreCase(vetor[j].getTitulo()) <= 0) {
-                aux[k] = vetor[i];
-                k++;
-                i++;
-            } else {
-                aux[k] = vetor[j];
-                k++;
-                j++;
-            }
-        }
-
-        while (i < meio) {
-            aux[k] = vetor[i];
-            k++;
-            i++;
-        }
-
-        while (j < tamanho) {
-            aux[k] = vetor[j];
-            k++;
-            j++;
-        }
-
-        for (i = inicio; i < tamanho; i++) {
-            vetor[i] = aux[i - inicio];
-        }
-    }
-
-    private static void intercalar(int inicio, int meio, int tamanho, Livro[] vetor) {
-        int i;
-        int j;
-        int k;
-
-        Livro[] aux = new Livro[tamanho - inicio];
-
-        i = inicio;
-        j = meio;
-        k = 0;
-
         while (i < meio && j < tamanho) {
-            if (vetor[i].getId() <= vetor[j].getId()) {
+            if (vetor[i].getTitulo().compareToIgnoreCase(vetor[j].getTitulo()) <= 0) {
                 aux[k] = vetor[i];
                 k++;
                 i++;
@@ -228,4 +121,6 @@ public class Operacoes {
             vetor[i] = aux[i - inicio];
         }
     }
+
+    
 }
--- 3_Semestre/Estrutura_Dados/Ado01/UsaLivro.java
+++ 3_Semestre/Estrutura_Dados/Ado01/UsaLivro.java
@@ -1,45 +1,23 @@
 public class UsaLivro {
-<<<<<<< Updated upstream
-    public static void main(String[] args) {
-        Livro livro01 = new Livro(15, "Matheus Arruda", "A", 15.90);
-        Livro livro02 = new Livro(20, "Arthur Arruda", "B", 19.90);
-        Livro livro03 = new Livro(25, "Gustavo Tozeti", "C", 10.00);
-        Livro livro04 = new Livro(30, "Fabiane Sanches", "D", 33.30);
-        Livro livro05 = new Livro(35, "Isabella Maria", "E", 40.00);
-        
-        Operacoes.inserir(livro03);
-=======
     public static void main(String[] args) throws Exception {
         
-        Livro livro03 = new Livro(25, "Gustavo Tozeti", "O Parca", 10.00);
-        Livro livro04 = new Livro(30, "Fabiane Sanches", "A Mae", 33.30);
-        Livro livro01 = new Livro(15, "Matheus Arruda", "O Brabo", 15.90);
-        Livro livro02 = new Livro(20, "Arthur Arruda", "O Irmao", 19.90);
-        Livro livro05 = new Livro(35, "Isabella Maria", "Minha Muie", 40.00);
+        Livro livro03 = new Livro(25, "Gustavo Tozeti", "A", 10.00);
+        Livro livro04 = new Livro(30, "Fabiane Sanches", "B", 33.30);
+        Livro livro01 = new Livro(15, "Matheus Arruda", "C", 15.90);
+        Livro livro02 = new Livro(20, "Arthur Arruda", "D", 19.90);
+        Livro livro05 = new Livro(35, "Isabella Maria", "E", 40.00);
         
         Operacoes.inserir(livro01);
->>>>>>> Stashed changes
         Operacoes.inserir(livro04);
         Operacoes.inserir(livro02);
         Operacoes.inserir(livro05);
-<<<<<<< Updated upstream
-        Operacoes.inserir(livro01);
-        Operacoes.inserir(livro02);
-=======
         Operacoes.inserir(livro03);
->>>>>>> Stashed changes
 
 
-<<<<<<< Updated upstream
-        System.out.println("");
-
-        Operacoes.sortByTitle();
-=======
         Operacoes.mostrar();
         System.out.println("\n\n");
 
-        Operacoes.sortById();
->>>>>>> Stashed changes
+        Operacoes.sortByTitulo();
         Operacoes.mostrar();
     }
 }
